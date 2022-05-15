882c78b3c7004ae35beea29cfd80e29cdbfbfc23
Yael Castro <yy.lgnd@gmail.com>
2022-05-14 22:33:05 -0500

    Refactoring to remove unnecessary code
    I made a refactor to reduce the code, get a better performace and added
    new features to be reuse the code later
    
    Changes made...
    
    1. Removed the Position data type and everything related to it because
       its values were replaced by binary values, so this data type is no
       longer needed
    
    2. The Draw State was replaced by the composite state by Player1Won and
       Plater2Won
    
    3. The Play method in Game returns composite states by Continue to
       better control of the current game state
    
    4. The NoState State was deleted because is no longer needed
    
    5. The Board has the Contains public method to help draw the board
    
    6. The range of positions on the board are between 0 and 8
    
    issue #1

--- internal/game/game.go
+++ internal/game/game.go
@@ -3,74 +3,65 @@ package game
 
 import "fmt"
 
-// Position indicates a position on Board
-type Position uint
-
-const (
-	// First row
-	// A indicates the first row and the first column
-	A Position = 1 << iota
-	// B indicates the first row and the second column
-	B
-	// C indicates the first row and the third column
-	C
-	// Second row
-	// D indicates the second row and the first column
-	D
-	// E indicates the second row and the second column
-	E
-	// F indicates the second row and the third column
-	F
-	// Third row
-	// G indicates the third row and the first column
-	G
-	// H indicates the third row and the second column
-	H
-	// I indicates the third row and the third column
-	I
-)
-
-// Board defines the bit mask for a Cat's Game player
+// Board is a bitmask used to represents a player board
+//
+// The board can be imagined as shown below... Where each space in the board can be selected by position
+//
+//  0  |  1  |  2
+//  3  |  4  |  5
+//  6  |  7  |  8
+//
+// But it is actually a bitmask of 9 bits that use each bit to represents a space in the board
+//
+// Board position | 8 7 6 5 4 3 2 1 0
+// Bitmask        | 0 0 0 0 0 0 0 0 1
+//
 type Board uint
 
 // IsFull returns true if all positions on the board all full
 func (b Board) IsFull() bool {
-	return int(b) == int(A|B|C|D|E|F|G|H|I)
+	return int(b) == int(0b111_111_111)
 }
 
-// contains indicates if a space on the board is occuped
-func (b Board) contains(p Position) bool {
-	return int(b)&int(p) == int(p)
+// Contains indicates if a position on the board is already taken
+func (b Board) Contains(bit uint) bool {
+	b2 := Board(1 << bit)
+	return b&b2 == b2
+}
+
+// contains indicates if a raw space on the board is occuped
+func (b Board) contains(b2 Board) bool {
+	return b&b2 == b2
 }
 
 // IsComplete indicates if the board has been completed
 func (b Board) IsComplete() bool {
 	switch {
 	// Horizontal cases
-	case b.contains(A | B | C):
+	case b.contains(0b000_000_111):
 		return true
 
-	case b.contains(D | E | F):
+	case b.contains(0b000_111_000):
 		return true
 
-	case b.contains(G | H | I):
+	case b.contains(0b111_000_000):
 		return true
 
 	// Vertical cases
-	case b.contains(A | D | G):
+	case b.contains(0b001_001_001):
 		return true
 
-	case b.contains(B | E | H):
+	case b.contains(0b010_010_010):
 		return true
 
-	case b.contains(C | F | I):
+	case b.contains(0b100_100_100):
 		return true
 
 	// Slash cases
-	case b.contains(A | E | I):
+	case b.contains(0b100_010_001):
 		return true
 
-	case b.contains(C | E | G):
+	case b.contains(0b001_010_100):
 		return true
 	}
 
@@ -87,12 +78,8 @@ func (s State) Is(state State) bool {
 
 // Supported values for State, defines the
 const (
-	// NoState must be used in the game begin
-	NoState State = 1 << iota
-	// Draw indicates the
-	Draw
 	// Continue indicates the success turn
-	Continue
+	Continue State = 1 << iota
 	// Player1Won indicates the player 1 victory
 	Player1Won
 	// Player2Won indicates the player 2 victory
@@ -134,34 +121,26 @@ func (g Game) Player2() Board {
 	return g.player2
 }
 
-// validatePosition check if the position is valid
-func (g Game) validatePosition(p Position) bool {
-	switch p {
-	case A, B, C, D, E, F, G, H, I:
-		return true
-	}
-
-	return false
-}
-
 // Play starts the game and continue using the current state
-func (g *Game) Play(p Position) State {
-	if !g.validatePosition(p) {
-		return InvalidTurn
+func (g *Game) Play(position uint) State {
+	if position > 8 {
+		return Continue | InvalidTurn
 	}
 
-	if (g.player1 | g.player2).contains(p) {
-		return NoSpace
+	b := Board(1 << position)
+
+	if (g.player1 | g.player2).contains(b) {
+		return Continue | NoSpace
 	}
 
 	// Player 1 turn
 	if g.Turn() {
-		g.player1 = g.player1 | Board(p)
+		g.player1 = g.player1 | b
 		goto checkResults
 	}
 
 	// Player 2 turn
-	g.player2 = g.player2 | Board(p)
+	g.player2 = g.player2 | b
 
 checkResults:
 	g.turn = !g.turn
@@ -175,7 +154,7 @@ checkResults:
 	}
 
 	if (g.player1 | g.player2).IsFull() {
-		return Draw
+		return Player1Won | Player2Won
 	}
 
 	return Continue
@@ -191,17 +170,17 @@ func (g *Game) Reset() {
 func (g Game) String() string {
 	out := ""
 
-	for i := 0; i < 9; i++ {
+	for i := uint(0); i < 9; i++ {
 		if i%3 == 0 {
 			out += "\n"
 		}
 
-		if g.player1.contains(1 << i) {
+		if g.player1.Contains(i) {
 			out += "  x  "
-		} else if g.player2.contains(1 << i) {
+		} else if g.player2.Contains(i) {
 			out += "  o  "
 		} else {
-			out += fmt.Sprintf("  %d  ", i+1)
+			out += fmt.Sprintf("  %d  ", i)
 		}
 	}
 
